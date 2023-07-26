77748a86e129a6503d2766358c422a9a71605f67
Lionel Cancellieri <lio2014vt@gmail.com>
2023-07-26 10:24:30 -0300

    agregamos 04-prueba

---
+++ 04-prueba/src/topics/07-import-export.ts
@@ -0,0 +1,24 @@
+
+import { Product, taxCalculation } from './06-function-destructuring';
+
+const shopingCart: Product[] = [
+
+    {
+        description: 'Nokia',
+        price: 100
+    },
+
+    {
+        description: 'iPad',
+        price: 150
+    }
+
+];
+
+const [total, tax] = taxCalculation({
+    products: shopingCart,
+    tax: 0.15
+ });
+
+console.log('Total', total);
+console.log('Tax', tax);
