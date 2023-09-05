8e34bc5b4f8c007e1f027799eae4395da3144cdd
Lionel Cancellieri <lio2014vt@gmail.com>
2023-09-05 17:35:36 -0300

    UPDATED: I18nSelectPipe: NOTEBOOK

--- 05-pipesApp/src/app/app.module.ts
+++ 05-pipesApp/src/app/app.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+
+// Configuración del locale de la App
+import localeEsAR from '@angular/common/locales/es-AR';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData( localeEsAR );
 
 
 @NgModule({
@@ -17,7 +23,9 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     SharedModule,
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-AR' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-uncommon-page',
-  templateUrl: './uncommon-page.component.html',
-  styleUrls: ['./uncommon-page.component.css']
+    selector: 'app-uncommon-page',
+    templateUrl: './uncommon-page.component.html',
+    styleUrls: ['./uncommon-page.component.css']
 })
+
+
 export class UncommonPageComponent {
+
+
+
+    // i18n Select
+    public name: string = 'Lionel';
+    public gender: 'male' | 'female' = 'male';
+    
+    public invitationMap = {
+        male: 'invitarlo',
+        female: 'invitarla'
+    }
+
+     changeClient(): void {
+        this.name = 'Fátima';
+        this.gender = 'female';
+     }
 
 }
