a27c72723a6776bbb71ac6d09d05d337055ac8a2
Lionel Cancellieri <lio2014vt@gmail.com>
2023-09-11 21:43:38 -0300

    UPDATED: Sección 11 terminada: NOTEBOOK

--- 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, interval, tap } from 'rxjs';
 
 @Component({
     selector: 'app-uncommon-page',
@@ -25,4 +26,37 @@ export class UncommonPageComponent {
         this.gender = 'female';
      }
 
+     // i18n Plural
+     public clients: string[] = ['Maria', 'Pedro', 'Fernando', 'Hernando', 'Eduardo', 'Melissa', 'Natalia'];
+     public clientsMap = {
+        '=0': 'no tenemos ningún cliente esperando.',
+        '=1': 'tenemos un cliente esperando.',
+        '=2': 'tenemos 2 clientes esperando.',
+        'other': 'tenemos # clientes esperando.'
+     }
+
+
+     deleteClient(): void {
+        this.clients.shift();
+     }
+
+     // KeyValue Pipe
+     public person = {
+        name: 'Lionel',
+        age: 28,
+        address: 'Santa Fe, Argentina'
+     }
+
+     //async Pipe
+     public myObservableTimer: Observable<number> = interval(2000).pipe(
+        tap( value => console.log('tap', value) )
+     );
+
+     public promiseValue: Promise<string> = new Promise( (resolve, reject) => {
+        setTimeout( () => {
+            resolve('Tenemos data en la promesa') 
+        }, 3500 )
+     })
+    
+     
 }
