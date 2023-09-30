9d63e2643d33c5f6138a25570cdc697c4084fc6f
lionel cancellieri <lio2014vt@gmail.com>
2023-09-30 14:18:02 -0300

    UPDATED: Fin seccion 15: PC

--- 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { switchMap, tap, filter } from 'rxjs';
 
-import { switchMap } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+
 import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { ConfirmDialogComponent } from '../../components/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-new-page',
@@ -35,7 +38,8 @@ export class NewPageComponent implements OnInit {
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private snackbar: MatSnackBar,
-  ) {}
+    private dialog: MatDialog
+  ) { }
 
   get currentHero(): Hero {
     const hero = this.heroForm.value as Hero;
@@ -44,18 +48,18 @@ export class NewPageComponent implements OnInit {
 
   ngOnInit(): void {
 
-      if( !this.router.url.includes('edit') ) return;
+    if (!this.router.url.includes('edit')) return;
 
-      this.activatedRoute.params
-        .pipe(
-          switchMap( ( {id} ) => this.heroesService.getHeroById(id) ))
-          .subscribe( hero => {
-            if ( !hero ) {
-              return this.router.navigateByUrl('/');
-            }
-            this.heroForm.reset(hero);
-            return;
-          } )
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.heroesService.getHeroById(id)))
+      .subscribe(hero => {
+        if (!hero) {
+          return this.router.navigateByUrl('/');
+        }
+        this.heroForm.reset(hero);
+        return;
+      })
 
   }
 
@@ -80,10 +84,41 @@ export class NewPageComponent implements OnInit {
       })
   }
 
-  showSnackbar( message: string ): void {
-      this.snackbar.open( message, 'done', {
-        duration: 2500
-      } )
+  onDeleteHero() {
+    if (!this.currentHero.id) throw Error('Hero id is required');
+
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: this.heroForm.value
+    });
+
+    dialogRef.afterClosed()
+      .pipe(
+        filter( (result: boolean) => result ),
+        switchMap( () => this.heroesService.deleteHeroById( this.currentHero.id ) ),
+        filter( (wasDeleted: boolean) => wasDeleted ),
+      )
+
+      .subscribe( () => {
+        this.router.navigate(['/heroes'])
+      });
+      
+
+    // dialogRef.afterClosed().subscribe(result => {
+    //     if( !result ) return;
+    //     this.heroesService.deleteHeroById(this.currentHero.id)
+    //       .subscribe( wasDeleted => {
+    //         if (wasDeleted)
+    //         this.router.navigate(['/heroes'])
+    //       } )
+    // });
+
+
+  }
+
+  showSnackbar(message: string): void {
+    this.snackbar.open(message, 'done', {
+      duration: 2500
+    })
   }
 
 }
