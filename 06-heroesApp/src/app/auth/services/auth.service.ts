90f572c592331a38390241b2d859b7958968a992
lionel cancellieri <lio2014vt@gmail.com>
2023-10-02 23:33:37 -0300

    UPDATED: AuthService - Servicio de Autenticación: PC

--- 06-heroesApp/src/app/auth/services/auth.service.ts
+++ 06-heroesApp/src/app/auth/services/auth.service.ts
@@ -1,15 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
 
 import { environments } from 'src/app/environments/environments';
+import { User } from '../interfaces/user.interface';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
 
   private baseURL = environments.baseUrl;
+  private user?: User;
 
   constructor(private http: HttpClient) { }
 
+  get currentUser(): User | undefined {
+    if ( !this.user ) return undefined;
+    return structuredClone(this.user); // structuredClone() cumple la misma funcion que el operador spread '...' pero mas profundo
+  }
+
+  login( email: string, password: string ): Observable<User> {
+    
+    return this.http.get<User>(`${this.baseURL}/users/1`)
+      .pipe(
+        tap( user => this.user = user ),
+        tap( user => localStorage.setItem('token', user.id.toString()) )
+      )
+  }
 
 }
 
