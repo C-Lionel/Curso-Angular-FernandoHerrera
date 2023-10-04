fb02bec0d85ec900335032dc194d096a21826c36
lionel cancellieri <lio2014vt@gmail.com>
2023-10-03 23:58:09 -0300

    UPDATED: Implementar la funcionalidad del Guard: PC

--- 06-heroesApp/src/app/auth/services/auth.service.ts
+++ 06-heroesApp/src/app/auth/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
+import { Observable, catchError, map, of, pipe, tap } from 'rxjs';
 
 import { environments } from 'src/app/environments/environments';
 import { User } from '../interfaces/user.interface';
@@ -19,12 +19,31 @@ export class AuthService {
   }
 
   login( email: string, password: string ): Observable<User> {
-    
+
     return this.http.get<User>(`${this.baseURL}/users/1`)
       .pipe(
         tap( user => this.user = user ),
-        tap( user => localStorage.setItem('token', user.id.toString()) )
+        tap( user => localStorage.setItem('token', 'slkdpfpñ.akpsdasdp.jaosdhoias') )
       )
+  }
+
+  checkAuthentication(): Observable<boolean> {
+
+    if (!localStorage.getItem('token')) return of(false) ;
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${this.baseURL}/users/1`)
+      .pipe(
+        tap( user => this.user = user ),
+        map( user => !!user ),
+        catchError( err => of(false) )
+      );
+  }
+
+  logout() {
+    this.user = undefined;
+    localStorage.clear()
   }
 
 }
