1f9836c398b8e20cb3ca7410584041c5951c0b15
lionel cancellieri <lio2014vt@gmail.com>
2023-10-04 23:02:24 -0300

    UPDATED: Fin sección 16: PC

--- 06-heroesApp/src/app/app-routing.module.ts
+++ 06-heroesApp/src/app/app-routing.module.ts
@@ -3,20 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
 
 //Importamos las dos funciones que creamos anteriormente.
-import { canActivateGuard, canMatchGuard } from './auth/guards/auth.guard';
+import { canActivateAuthGuard, canMatchAuthGuard } from './auth/guards/auth.guard';
+import { canActivatePublicGuard, canMatchPublicGuard } from './auth/guards/public.guard';
+
 
 
 
 const routes: Routes = [
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    canActivate: [canActivatePublicGuard], //Anclamos la función del canActive
+    canMatch: [canMatchPublicGuard], //Anclamos la función del canMatch
   },
   {
     path: 'heroes',
     loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule),
-    canActivate: [canActivateGuard], //Anclamos la función del canActive
-    canMatch: [canMatchGuard], //Anclamos la función del canMatch
+    canActivate: [canActivateAuthGuard], //Anclamos la función del canActive
+    canMatch: [canMatchAuthGuard], //Anclamos la función del canMatch
   },
   {
     path: '404',
--- 06-heroesApp/src/app/auth/guards/public.guard.ts
+++ 06-heroesApp/src/app/auth/guards/public.guard.ts
@@ -1,6 +1,6 @@
 //se importa esta libreria para poder inyectar dependencias sin constructor de clase
 import { inject } from '@angular/core';
-import { Observable, tap, pipe } from 'rxjs';
+import { Observable, tap, pipe, map } from 'rxjs';
 import {ActivatedRouteSnapshot, CanActivateFn, CanMatchFn, Route, Router, RouterStateSnapshot, UrlSegment,} from '@angular/router';
 
 import { AuthService } from '../services/auth.service';
@@ -15,27 +15,26 @@ const checkAuthStatus = (): boolean | Observable<boolean> => {
   return authService.checkAuthentication().pipe(
     tap(isAuthenticated => console.log('Authenticated', isAuthenticated)),
     tap((isAuthenticated) => {
-      if (!isAuthenticated) {
-        router.navigate(['/auth/login']);
+      if (isAuthenticated) {
+        router.navigate(['./']);
       }
-    })
+    }),
+    map( isAuthenticated => !isAuthenticated)
   );
 };
 
 //No hay necesidad de crear una clase, simplemente definiendo una función flecha y exportándola podemos utilizar sus funcionalidades de guard en el app-routing
 
-export const canActivateAuthGuard: CanActivateFn = ( route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
+export const canActivatePublicGuard: CanActivateFn = ( route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
   console.log('CanActivate');
   console.log({ route, state });
 
   return checkAuthStatus();
 };
 
-export const canMatchAuthGuard: CanMatchFn = ( route: Route, segments: UrlSegment[] ) => {
+export const canMatchPublicGuard: CanMatchFn = ( route: Route, segments: UrlSegment[] ) => {
   console.log('CanMatch');
   console.log({ route, segments });
 
   return checkAuthStatus();
 };
-
-
