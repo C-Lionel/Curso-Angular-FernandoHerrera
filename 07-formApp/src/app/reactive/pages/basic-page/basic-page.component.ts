6274cad9d98992f24a02e3238166b6a4a22b1ed1
Lionel Cancellieri <lio2014vt@gmail.com>
2023-10-09 12:54:34 -0300

    updated

--- 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   ]
 })
 export class BasicPageComponent {
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
+
+  // formBuilder una manera mas simplificada de trabajar con formulario reactivos que reemplaza a FormGroup() (aunque no está obsoleto)
+
+  public myForm: FormGroup = this.formBuilder.group({
+        name: ['', [ Validators.required, Validators.minLength(3) ]],
+        price: [0, [ Validators.required, Validators.min(0) ]],
+        inStorage: [0, [ Validators.required, Validators.min(0) ]]
+  })
+
+  onSave(): void {
+    if(this.myForm.invalid) return;
+    
+    console.log(this.myForm.value)
+  }
 
 }
