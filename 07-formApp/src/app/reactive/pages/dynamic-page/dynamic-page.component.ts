9481012df0eede0747f095cc82525a7afd0f93b5
lionel cancellieri <lio2014vt@gmail.com>
2023-10-12 00:30:35 -0300

    Updated: PC

--- 07-formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 07-formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class DynamicPageComponent {
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  public myForm: FormGroup = this.formBuilder.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.formBuilder.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required],
+    ])
+  })
+
+  onSubmit(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return
+    }
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
+
 }
+
+
