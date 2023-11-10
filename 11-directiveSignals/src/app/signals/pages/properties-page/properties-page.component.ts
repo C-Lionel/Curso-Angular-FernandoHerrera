d8749203aad7981eeb17ef9cbde8503f7db56137
lionel cancellieri <lio2014vt@gmail.com>
2023-11-10 19:35:37 -0300

    UPDATED: Fin sección 24: PC

--- 11-directiveSignals/src/app/signals/pages/properties-page/properties-page.component.ts
+++ 11-directiveSignals/src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-properties-page',
@@ -7,8 +8,46 @@ import { Component } from '@angular/core';
 })
 export class PropertiesPageComponent {
 
-  onFieldUpdated( field: string, value: string ) {
-    console.log( field, value )
+  public user = signal<User>({
+    id: 1,
+    email: 'Lio2014vt@gmail.com',
+    first_name: 'Lionel',
+    last_name: 'Cancellieri',
+    avatar: 'https://reqres.in/img/faces/1-image.jpg'
+  });
+
+  public counter = signal(10);
+
+  increaseBy( value: number ) {
+    this.counter.update( current => current + value )
+  }
+
+  public userChangedEffect = effect( () => {
+    console.log( `${this.user().first_name} - ${this.counter()}` )
+  } )
+
+  onFieldUpdated( field: keyof User, value: string ) {
+
+    this.user.mutate( current => {
+      switch( field ) {
+
+        case 'email':
+          current.email = value;
+        break;
+
+        case 'first_name':
+          current.first_name = value;
+        break;
+
+        case 'last_name':
+          current.last_name = value;
+        break;
+
+        case 'id':
+          current.id = Number(value);
+        break;
+      }
+    })
   }
 
 }
